6b292d83a908c47f3cf822fccbed72f1fdff9468
차한음 <ihaneum@chahan-eum-ui-MacBookAir.local>
2021-06-15 19:03:46 +0900

    v7.6.2

--- exec.py
+++ exec.py
@@ -17,30 +17,29 @@ def Isdir(path):
         return 1
     else:
         return -1
-def Lps(p):
-    n = len(p)
+def Table(text):
+    n = len(text)
     table = [0] * n
-    k = 0
+    m = 0
     for i in range(1, n):
-        while k > 0 and p[i] != p[k]:
-            k = table[k - 1]
-        if p[i] == p[k]:
-            k += 1
-            table[i] = k
+        m = table[m-1]
+    if text[i] == p[m]:
+        m += 1
+        table[i] == m
     return table
-def Find(s, p):
-    k = 0
-    c = 0
-    table = Lps(p)
-    for i in range(len(s)):
-        while k > 0 and s[i] != p[k]:
-            k = table[k - 1]
-        if s[i] == p[k]:
-            k += 1
-            if k == len(p):
-                c = 1
-                k = table[k - 1]
-    return c # 0이나 1만 반환되요
+def Find(target, text):
+    c = 0 # count
+    m = 0
+    table = Table(text)
+    for i in range(len(target)):
+        while m > 0 and target[i] != text[m]:
+            m = table[m-1]
+        if target[i] == text[m]:
+            m += 1
+            if m == len(text):
+                c += 1
+                m = table[m-1]
+        return c
 def FindFile(path, text):
     isfile = Isfile(path)
     if isfile == -1:
--- tests/kmp2.py
+++ tests/kmp2.py
@@ -13,21 +13,23 @@ def Dup(p): # a
             j += 1 # j=1 # 문자열 일치 개수 # 1씩 커짐
             table[i] = j # [0,..]에 1을 추가해요
             print("j: %s"%j)
-    return table
+    return table # 1이나 0을 반환해요 # 0
 
 def Kmp(s, p):
     j = 0 # 패턴 인덱스 
     cnt = 0 # 일치 개수 
-    table = Dup(p)
-    for i in range(len(s)):
+    table = Dup(p) # [0]
+    print("table2: %s"%table)
+    for i in range(len(s)): # 2
+        print("i2: %s"%i)
         while j > 0 and s[i] != p[j]:
             j = table[j - 1]
-        if s[i] == p[j]:
+        if s[i] == p[j]: # a==a
             j += 1
             if j == len(p): # 일치
                 cnt += 1
-                j = table[j - 1]
-    return cnt
+                j = table[j - 1] # j = 0
+    return cnt # 일치 개수를 반환해요
 
 s = input() # abc
 p = input() # a
